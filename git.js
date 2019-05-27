e12f9c1a007756bec139d0d82b2fa010102292e8
onurcck <onurcck@gmail.com>
2019-05-27 10:58:22 +0300

    tamamlandı

--- git.js
+++ git.js
@@ -7,3 +7,58 @@
 // git config --global alias.co checkout
 
 // bunları girdikten sonra hesapla eşlemşme uyapar
+
+//C: \Users\O & E\Desktop\node - js - giris\web - server > ls 
+// ls çekerek buunduğumuz klasör içerisindeki dosyalaırmıızı görüntüleyeibliriz.
+
+
+//git init diyerek git ile ilgili repository burda kurarız
+//Initialized empty Git repository in C:/Users/O&E/Desktop/node-js-giris/web-server/.git/ böyle bir uyarı gelir
+
+//--------------------------------------------------------------------------------------------
+// git status diyerek kırmızı dosyalar görünür bunlar yeni eklenmiş dosyalar demektir
+/*On branch master
+
+No commits yet
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+
+        git.js
+        node_modules/
+        package-lock.json
+        package.json
+        public/
+        server.js
+
+nothing added to commit but untracked files present (use "git add" to track)
+*/
+//------------------------------------------------------------------------------------------------------------
+
+//diyelimki bişeyler silmek istioyuruz ozaman şunu yaparız
+
+//git rm --cached -r node_modules  // node modules klasörünü sileceketir.
+
+// tekrar git status diye yazarsak sildiği klasörüde kırmızı olarak aşağıda gösterecektir.
+
+/*
+Changes to be committed:
+  (use "git rm --cached <file>..." to unstage)
+
+        new file:   git.js
+        new file:   package-lock.json
+        new file:   package.json
+        new file:   public/index.html
+        new file:   server.js
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+
+        node_modules/ // burası kırmızı görünür
+
+*/
+
+// eğer bu kırmızı sildiklerimizi görmek istemiyorsak yoksayacağız bunuda şu şekilde yapabiliriz
+
+
+//şimdi gitti bi tane dosya açtık .gitignore diye içinede node_modules/ yazdık içine yazdıklarımızı görmezden gel demek
